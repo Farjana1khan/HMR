56df17b10168ad1b17e0921921454e47b7d6d802
Farjana Khan <farjana09khan@gmail.com.com>
2022-09-02 17:40:12 +0530

    commit message

--- src/index.js
+++ src/index.js
@@ -9,7 +9,20 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  
 );
+
+if(module.hot){
+  module.hot.accept('./App', ()=>{
+    const NextApp = require('./App').default;
+    root.render(
+    
+      <React.StrictMode>
+ <NextApp />
+      </React.StrictMode>
+    );
+  })
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
